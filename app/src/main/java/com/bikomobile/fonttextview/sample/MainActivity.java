b738ffd1dbcdb168707bcf6e09ccedf3b9336b4c
Javier González <javier.gonzalez@bikomobile.com>
2016-08-10 13:58:20 +0200

    Added more examples

--- app/src/main/java/com/bikomobile/fonttextview/sample/MainActivity.java
+++ app/src/main/java/com/bikomobile/fonttextview/sample/MainActivity.java
@@ -2,6 +2,10 @@ package com.bikomobile.fonttextview.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.bikomobile.widget.fontview.FontTextView;
+import com.bikomobile.widget.fontview.FontUtils;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FontTextView fontTextView = (FontTextView) findViewById(R.id.font_text_view);
+        fontTextView.setFontPath("fonts/Ubuntu-Regular.ttf");
+        fontTextView.setText("Programmatically");
+
+        Button button = (Button) findViewById(R.id.button);
+        FontUtils.getInstance().replaceFontFromAsset(button, "fonts/Ubuntu-Regular.ttf");
+        button.setText("Button");
 
     }
 }
